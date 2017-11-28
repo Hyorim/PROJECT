ef257235df885cc48dad0da5ba4bc02ba2e05ff5
minju Park <wkaustjzzz@gmail.com>
2017-11-28 05:42:39 -0800

    bingo_print

---
+++ lib/bingo_print.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int bingo_print(int(*arr)[5])
+{
+        int i,j;
+        for (i=0;i<5;i++)
+        {
+                for (j=0;j<5;j++)
+                {
+                        if(arr[i][j]==-1)
+                        {
+                                printf(" O ");
+                        }
+                        else
+                        {
+                                printf(" %d ",arr[i][j]);
+                        }
+                }
+                printf("\n");
+	}
+}
