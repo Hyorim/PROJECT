c7b58ba9c0432cd42e8d19805f1a1c684e8a6656
minju Park <wkaustjzzz@gmail.com>
2017-11-30 10:47:57 -0800

    Change sort numbers

--- lib/bingo_print.c
+++ lib/bingo_print.c
@@ -13,9 +13,29 @@ int bingo_print(int(*arr)[5])
                         }
                         else
                         {
-                                printf(" %d ",arr[i][j]);
+                                printf("%2d ",arr[i][j]);
                         }
                 }
                 printf("\n");
 	}
+}
+
+int main()
+{
+	int i,j;
+	int z=1;
+	int arr[5][5];
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			arr[i][j]=z;
+			z++;
+		}
+	}
+	arr[1][3]=-1;
+	arr[2][2]=-1;
+	arr[0][1]=-1;
+	arr[4][2]=-1;
+	bingo_print(arr);
 }
