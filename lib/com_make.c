040ddd3a17faf9e18ce5c9e30ee823f685801d1f
minju Park <wkaustjzzz@gmail.com>
2017-11-30 23:23:01 -0800

    com_number range 1~50

--- lib/com_make.c
+++ lib/com_make.c
@@ -2,27 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
-int temp[25]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
-
-int check()
-{
-	int i;
-	for(i=0;i<25;i++)
-	{
-		if(temp[i]==-1)
-		{
-			return 0;
-		}
-	}
-	return 1;
-}
+int temp[50]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
 
 void com_make(int(*array)[5])
 {
 
 	int i=0;
+	int k=0;
 	srand((int)time(NULL));
-	while(!check())
+	while(k<25)
 	{
 		int num=(rand()%25)+1;
 		if(temp[num-1]==-1)
@@ -30,6 +18,7 @@ void com_make(int(*array)[5])
 			array[i/5][i%5]=num;
 			temp[num-1]=0;
 			i++;
+			k++;
 		}
 	}
 }
